a1899dafa283a08e0196b70f789f4f05614e6fe4
suraches <suraches.see@ascendcorp.com>
2021-05-18 20:16:58 +0700

    update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
                     <ul class="social-icons two">
                         <!-- <li><a href="#"> </a></li> -->
                         <li><a target="_blank" href="https://www.facebook.com/suraches.seesat" class="fb"> </a></li>
-                        <!-- <li><a target="_blank" href="https://plus.google.com/u/0/+SurachesSeesat" class="in"> </a></li> -->
+                        <li><a target="_blank" href="https://plus.google.com/u/0/+SurachesSeesat" class="in"> </a></li>
                         <li><a target="_blank" href="http://www.inewgent.com" class="dott"> </a></li>
                     </ul>
                 </div>
